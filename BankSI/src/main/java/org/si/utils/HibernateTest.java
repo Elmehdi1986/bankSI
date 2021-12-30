224a9096776a726a0a34f496d9b2bc6dafcedd9b
Elmehdi MATRANE <elmehdi.matrane@gmail.com>
2021-12-30 10:11:55 -0500

    add main method to the hibernate test
    	# with '#' will be ignored, and an empty message aborts the commit.
    	# interactive rebase in progress; onto 80146f4

--- BankSI/src/main/java/org/si/utils/HibernateTest.java
+++ BankSI/src/main/java/org/si/utils/HibernateTest.java
@@ -1,5 +1,9 @@
 package org.si.utils;
 
 public class HibernateTest {
-System.out.println("first Paragraph");
+	
+	public static void main(String[] args) {
+	    System.out.println("first Paragraph");
+		System.out.println("add text 3");
+	}
 }
