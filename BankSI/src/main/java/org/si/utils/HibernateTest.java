a82d5505b94803208d1087a61cc8ab66d33186c0
Elmehdi1986 <77241418+Elmehdi1986@users.noreply.github.com>
2021-12-30 10:09:40 -0500

    Update HibernateTest.java
    add text to test conflict

--- BankSI/src/main/java/org/si/utils/HibernateTest.java
+++ BankSI/src/main/java/org/si/utils/HibernateTest.java
@@ -4,6 +4,9 @@ public class HibernateTest {
 	
 	public static void main(String[] args) {
 	    System.out.println("first Paragraph");
+	    System.out.println("add text 1 ");
+	    System.out.println("add text 2");
 		System.out.println("add text 3");
 	}
+
 }
